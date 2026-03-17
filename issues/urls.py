942fa618a474549d578ae063bc0fbd25aa37bb29
Adam Ziani <adamziani@MacBook-Pro-de-Adam.local>
2026-03-17 23:28:03 +0100

    feat: add issue_new view and urls

--- issues/urls.py
+++ issues/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('issues/', views.issue_list, name='issue_list'),
+    path('issues/new/', views.issue_new, name='issue_new'),
+]
--- issues/views.py
+++ issues/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Issue
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def issue_list(request): 
     issues = Issue.objects.all()
     return render(request, 'issues/issue_list.html', {'issues': issues})
+
+def issue_new(request):
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        description = request.POST.get('description', '') #por si descripción vacía
+        user = User.objects.get(id = 1) #por defecto, falta por configurar
+        Issue.objects.create(subject = subject, description = description, created_by = user)
+        return redirect('issue_list') #lo tengo en URLs
+    return render(request, 'issues/issue_new.html') #si acaba de entrar a esa página(GET)
+
+#no inicializo otros atrbiutos porque ya tienen valor por defecto de momento
