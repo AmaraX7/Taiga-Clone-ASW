23f7bd958b788567edc141d4f7bd2dcfc136c9ee
Victor Carilla <victor@blumb.ai>
2026-03-18 11:01:06 +0100

    User stories: view and add issues fet

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Issue
+from .forms import IssueForm
 
-# Create your views here.
+
+def get_default_user():
+    return User.objects.get(username='admin')
+
+
+def issue_list(request):
+    issues = Issue.objects.all()
+    return render(request, 'issues/list.html', {'issues': issues})
+
+
+def issue_new(request):
+    if request.method == 'POST':
+        form = IssueForm(request.POST)
+        if form.is_valid():
+            issue = form.save(commit=False)
+            user = get_default_user()
+            issue.created_by = user
+            issue.assigned_to = user
+            issue.save()
+            return redirect('issue_list')
+    else:
+        form = IssueForm()
+    return render(request, 'issues/new.html', {'form': form})
